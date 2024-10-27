f19ff3e2326611dbf095a33d28bed24fb3f60ac8
GesT-DEVELOPER <leonciklesa9@gmail.com>
2024-10-27 17:36:47 +0500

    Update SPEEDANDJUMP.lua

--- SPEEDANDJUMP.lua
+++ SPEEDANDJUMP.lua
@@ -1,4 +1,5 @@
 while wait() do
-	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 50
-	game.Players.LocalPlayer.Character.Humanoid.JumpHeight = 30
+	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 35
+	game.Players.LocalPlayer.Character.Humanoid.JumpHeight = 50
+	game.Players.LocalPlayer.Character.Humanoid.JumpPower = 50
 end
